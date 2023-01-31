d2faddc43d2439caa870e27e3e8423062c88a351
jiaohan <3317565482@qq.com>
2023-01-31 16:00:15 +0800

    hot-fix commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -1,13 +1,13 @@
 package com.atguigu.git;
 
 /**
- * @author Ji_ah
- * @create 2023-01-31 15:30
+ *
  */
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello,git!");
         System.out.println("hello,git2!");
         System.out.println("hello,git3!");
+        System.out.println("hello,git4!");
     }
 }
