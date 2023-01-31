44271e2843ac5ad543a1b96a9f6365722fd108fc
jiaohan <3317565482@qq.com>
2023-01-31 15:35:08 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author Ji_ah
+ * @create 2023-01-31 15:30
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,git!");
+    }
+}
